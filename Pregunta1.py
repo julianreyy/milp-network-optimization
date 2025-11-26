bd790555be1cde989ea4485203a0560b17c3f4c5
mdc1000 <mariodelcastillo1001@gmail.com>
2025-11-26 23:03:18 +0100

    demanda cambiado

--- Pregunta1.py
+++ Pregunta1.py
@@ -73,7 +73,7 @@ for i in I:
 # Demanda de cada destino
 for j in J:
     m1.addConstr(
-        gp.quicksum(x[i, j] for i in I) <= d[j],
+        gp.quicksum(x[i, j] for i in I) == d[j],
         name=f"demanda_{j}"
     )
 
